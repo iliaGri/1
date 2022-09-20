393c75921aa572ee658ab798a38317e59065100f
Ilia <9995194032@mail.ru>
2022-09-21 01:49:44 +0300

    add file

---
+++ NEW/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+string[] array1 = new string[10] {"2546", "3455", "HEL", "L", "O", "WO", "RL", "D", "12648", "66655"};
+string[] array2 = new string[array1.Length];
+void Create(string[] array1, string[] array2)
+{
+int count = 0;
+for(int i = 0; i < array1.Length; i++)
+
+
+
+
+
+ {
+  if(array1[i].Length <= 3)
+    {
+    array2[count] = array1[i];
+    count++;
+    }
+ }
+}
+
+
+
+
+void Show(string[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]}.");
+}
+Console.WriteLine();
+}
+
+Create(array1, array2);
+Show(array2);
